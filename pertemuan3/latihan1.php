9faa99f566aaf906784774e44c790792af4066bc
Hanif Wihananto <hanifabid19@gmail.com>
2021-07-28 10:59:52 +0700

    pertemuan 3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,30 @@
+<?php 
+// Perulangan
+// for
+// while
+// do..while
+// foreach : Perulangan khusus array
+
+// for
+for ($i=0; $i < 5; $i++) { 
+	echo "Hello World! <br>";
+}
+echo "<br>";
+
+// while
+$i = 0;
+while ($i < 5) {
+	echo "Hallo Dunia! <br>";
+	$i++;
+}
+echo "<br>";
+
+$i = 0;
+// do..while
+do {
+	echo "Haii World! <br>";
+	$i++;
+} while ($i < 5);
+echo "<br>";
+
+ ?>
