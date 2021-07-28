51742bbaea329479e7da6d2d58f9ceda3fe409d3
Hanif Wihananto <hanifabid19@gmail.com>
2021-07-28 10:58:51 +0700

    Update UI

--- index.php
+++ index.php
@@ -1,3 +1,112 @@
-<?php 
-echo "Hello World";
- ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BELAJAR PHP | HANIF</title>
+
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/0b20dfeace.js" crossorigin="anonymous"></script>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap');
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: poppins, sans-serif;
+        }
+
+        header {
+            background-color: skyblue;
+            padding: 4px;
+            padding-left: 10px;
+        }
+
+        .tabel {
+            margin: 50px;
+            left: 20px;
+            /*border: 1px solid black;*/
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        td {
+            padding: 10px 50px;
+            text-align: center;
+        }
+
+
+    </style>
+</head>
+<body>
+    <!-- Header -->
+    <header>
+        <h1>BELAJAR PHP DASAR</h1>
+        <p>Sumber : YT Web Programming Unpas</p>
+    </header>
+
+    <!-- Bagian Kiri -->
+    <div class="tabel kiri">
+        <table>
+            <tr>
+                <th>No</th>
+                <th>Kegiatan</th>
+                <th>File</th>
+            </tr>
+
+            <tr>
+                <td>1.</td>
+                <td>Pertemuan 1</td>
+                <td><a target="_blank" href="pertemuan1/index.php">index.php</a></td>
+            </tr>
+
+            <tr>
+                <td>2.</td>
+                <td>Pertemuan 2</td>
+                <td><a target="_blank" href="pertemuan2/index.php">index.php</a></td>
+            </tr>
+
+            <tr>
+                <td>3.</td>
+                <td>Pertemuan 3</td>
+                <td>
+                    <a target="_blank" href="pertemuan3/latihan1.php">latihan1.php</a><br>
+                    <a target="_blank" href="pertemuan3/latihan2.php">latihan2.php</a><br>
+                    <a target="_blank" href="pertemuan3/latihan3.php">latihan3.php</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>4.</td>
+                <td>Pertemuan 4</td>
+                <td>
+                    <a target="_blank" href="pertemuan4/latihan1.php">latihan1.php</a><br>
+                    <a target="_blank" href="pertemuan4/latihan2.php">latihan2.php</a><br>
+                    <a target="_blank" href="pertemuan4/latihan3.php">latihan3.php</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>5.</td>
+                <td>Pertemuan 5</td>
+                <td>
+                    <a target="_blank" href="pertemuan5/latihan1.php">latihan1.php</a><br>
+                    <a target="_blank" href="pertemuan5/latihan2.php">latihan2.php</a><br>
+                    <a target="_blank" href="pertemuan5/latihan3.php">latihan3.php</a>
+                </td>
+            </tr>
+
+
+        </table>
+        
+    </div>
+
+    <!-- Bagian Kanan -->
+    <div class="kanan">
+        
+    </div>
+
+</body>
+</html>
