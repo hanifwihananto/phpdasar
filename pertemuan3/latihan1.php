a461fe74f5b6010c8ffcb20c6c5faa15e8c4dfe7
Hanif <hanifabid19@gmail.com>
2021-07-28 13:31:48 +0700

    pertemuan 3

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -1,4 +1,43 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Latihan 1</title>
+
+	<style>
+		.warna-baris {
+			background-color: silver;
+		}
+	</style>
+</head>
+
+<body>
+	<h3>Example :</h3>
+
+	<table border="1" cellpadding="10" cellspacing="0">
+		<?php for ($i = 1; $i <= 5; $i++) : ?>
+			<?php if ($i % 2 == 1) : ?>
+				<tr class="warna-baris">
+				<?php else : ?>
+				<tr>
+				<?php endif; ?>
+
+				<?php for ($j = 1; $j <= 5; $j++) : ?>
+					<td><?php echo "$i, $j" ?> </td>
+				<?php endfor; ?>
+				</tr>
+			<?php endfor; ?>
+	</table>
+	<br>
+
+</body>
+
+</html>
+
+<?php
 // Perulangan
 // for
 // while
@@ -6,7 +45,7 @@
 // foreach : Perulangan khusus array
 
 // for
-for ($i=0; $i < 5; $i++) { 
+for ($i = 0; $i < 5; $i++) {
 	echo "Hello World! <br>";
 }
 echo "<br>";
@@ -19,12 +58,11 @@ while ($i < 5) {
 }
 echo "<br>";
 
-$i = 0;
+$i = 1;
 // do..while
 do {
-	echo "Haii World! <br>";
+	echo $i . ". Haii World! <br>";
 	$i++;
 } while ($i < 5);
 echo "<br>";
-
- ?>
+?>
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,15 @@
+<?php
+// Pengkondisian / Percabangan
+// if else
+// if else if
+// ternary
+// switch
+
+$x = 10;
+if ($x < 20) {
+    echo "Benar";
+} else if ($x == 20) {
+    echo "Nice";
+} else {
+    echo "Salah";
+}
