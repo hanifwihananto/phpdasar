8f88cd0d0cd81566ad1bbd351162fbda418b0e0f
Hanif Wihananto <hanifabid19@gmail.com>
2021-07-30 16:48:50 +0700

    pertemuan 7

--- pertemuan7/login/login.php
+++ pertemuan7/login/login.php
@@ -1,3 +1,18 @@
+<?php 
+// Cek apakah tombol submit sudah ditekan atau belum
+    if (isset($_POST["submit"])) {
+        // Cek user & password
+        if ($_POST["username"] == "admin" && 
+            $_POST["password"] == "123") {
+            // Jika benar, redirect kehalaman admin
+            header("Location: admin.php");
+            exit;
+        } else {
+            // Jika salah, tampilkan pesan kesalahan
+            $error = true; 
+        }   
+    }
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,15 +20,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <style>
+        ul {
+            list-style-type: none;
+        }
+        .error {
+            color: red;
+            font-style: italic;
+        }
+    </style>
 </head>
 <body>
     <h1>Login Admin</h1>
 
+    <?php if (isset($error)) : ?>
+        <p class="error">Username / Password salah!</p>
+    <?php endif; ?>
+    
     <ul>
         <form action="" method="post"> 
             <li>
                 <label for="username">Username : </label>
                 <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="password">Password : </label>
+                <input type="password" name="password" id="password">
+            </li>
+            <li>
+                <button type="submit" name="submit">Login</button>
             </li>
         </form>
     </ul>
